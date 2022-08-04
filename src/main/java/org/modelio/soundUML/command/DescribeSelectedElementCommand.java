1fe3e641d08f908a15ad6918eb2d0bdc07c704d0
Guerreiro <n.guerreiro@campus.fct.unl.pt>
2022-08-04 16:40:50 +0100

    Git Initial Commit - Sound playback is happening
    Basic functionality of sound playback is working, basic user text query is also working

---
+++ src/main/java/org/modelio/soundUML/command/DescribeSelectedElementCommand.java
@@ -0,0 +1,110 @@
+package org.modelio.soundUML.command;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.modelio.api.modelio.IModelioServices;
+import org.modelio.api.modelio.Modelio;
+import org.modelio.api.modelio.model.IModelingSession;
+import org.modelio.api.modelio.picking.IPickingClient;
+import org.modelio.api.modelio.picking.IPickingProvider;
+import org.modelio.api.modelio.picking.IPickingService;
+import org.modelio.api.modelio.picking.IPickingSession;
+import org.modelio.api.module.IModule;
+import org.modelio.api.module.command.DefaultModuleCommandHandler;
+import org.modelio.api.module.context.configuration.IModuleUserConfiguration;
+import org.modelio.api.module.context.log.ILogService;
+import org.modelio.metamodel.diagrams.AbstractDiagram;
+import org.modelio.metamodel.uml.infrastructure.ModelElement;
+import org.modelio.soundUML.impl.SimpleAudioPlayer;
+import org.modelio.vcore.smkernel.mapi.MObject;
+
+/**
+ * Implementation of the IModuleContextualCommand interface.
+ * <br>The module contextual commands are displayed in the contextual menu and in the specific toolbar of each module property page.
+ * <br>The developer may inherit the DefaultModuleContextualCommand class which contains a default standard contextual command implementation.
+ *
+ */
+public class DescribeSelectedElementCommand extends DefaultModuleCommandHandler {
+    /**
+     * Constructor.
+     */
+    public DescribeSelectedElementCommand() {
+        super();
+    }
+
+    /**
+     * @see org.modelio.api.module.commands.DefaultModuleContextualCommand#accept(java.util.List,
+     *      org.modelio.api.module.IModule)
+     */
+    @Override
+    public boolean accept(List<MObject> selectedElements, IModule module) {
+        // Check that there is only one selected element
+        return selectedElements.size() == 1;
+    }
+    
+
+    
+    /**
+     * @see org.modelio.api.module.commands.DefaultModuleContextualCommand#actionPerformed(java.util.List,
+     *      org.modelio.api.module.IModule)
+     */
+    @Override
+    public void actionPerformed(List<MObject> selectedElements, IModule module) {
+        ILogService logService = module.getModuleContext().getLogService();
+        logService.info("DescribeSelectedElementCommand - actionPerformed(...)");
+
+        IModelingSession session = module.getModuleContext().getModelingSession();
+        List<MObject> root = session.getModel().getModelRoots();
+        IModuleUserConfiguration configuration = module.getModuleContext().getConfiguration();
+
+        ModelElement modelelt = (ModelElement)selectedElements.get(0);            
+        
+        //List<? extends MObject> elements = modelelt.getCompositionChildren();
+       
+        //Chama o resource para tocar o ficheiro de audio
+        //Nao da para criar ficheiro através de um URI num ficheiro jar, mas dá para passar o input stream directamente e tocar
+        InputStream inputStream = getClass().getResourceAsStream("/org/modelio/soundUML/sounds/arrow.wav");
+        SimpleAudioPlayer player = new SimpleAudioPlayer();
+        player.play(inputStream);
+
+        
+        //Pede o input ao user
+        
+        String teste = JOptionPane.showInputDialog(null, "Introduz texto aqui:");
+        MessageDialog.openInformation(null, "Teste string", teste);
+        
+        if(teste.equalsIgnoreCase("boas")) {
+            MessageDialog.openInformation(null, "Entrei aqui", modelelt.getName());
+        } else {
+            MessageDialog.openInformation(null, "Nao entrei", modelelt.getName());
+        }
+        
+
+        
+        //Evento em que vou clickar num elemento do diagrama, espera pelo click do user
+        
+        //IPickingService pickingService = module.getModuleContext().getModelioServices().getPickingService();
+        //IPickingClient pickingClient = null;
+        //IPickingSession pickingSession = pickingService.startPickingSession(pickingClient);
+
+   
+        //será que ele já seleciona e nem preciso de fazer isto?
+        
+        //Após click, ler informação daquele elemento em concreto com TTS
+        
+
+    }
+    
+   
+
+ }
+
---
+++ src/main/java/org/modelio/soundUML/command/FindElementInDiagramCommand.java
@@ -0,0 +1,113 @@
+package org.modelio.soundUML.command;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.modelio.api.modelio.model.IModelingSession;
+import org.modelio.api.module.IModule;
+import org.modelio.api.module.command.DefaultModuleCommandHandler;
+import org.modelio.api.module.context.configuration.IModuleUserConfiguration;
+import org.modelio.api.module.context.log.ILogService;
+import org.modelio.metamodel.diagrams.AbstractDiagram;
+import org.modelio.metamodel.uml.infrastructure.Element;
+import org.modelio.metamodel.uml.infrastructure.ModelElement;
+import org.modelio.vcore.model.CompositionGetter;
+import org.modelio.vcore.smkernel.mapi.MObject;
+
+/**
+ * Implementation of the IModuleContextualCommand interface.
+ * <br>The module contextual commands are displayed in the contextual menu and in the specific toolbar of each module property page.
+ * <br>The developer may inherit the DefaultModuleContextualCommand class which contains a default standard contextual command implementation.
+ *
+ */
+public class FindElementInDiagramCommand extends DefaultModuleCommandHandler {
+	
+    /**
+     * Constructor.
+     */
+    public FindElementInDiagramCommand() {
+        super();
+    }
+
+    /**
+     * @see org.modelio.api.module.commands.DefaultModuleContextualCommand#accept(java.util.List,
+     *      org.modelio.api.module.IModule)
+     */
+    @Override
+    public boolean accept(List<MObject> selectedElements, IModule module) {
+        // Check that there is only one selected element
+        return selectedElements.size() == 1;
+    }
+    
+   
+
+
+    /**
+     * @see org.modelio.api.module.commands.DefaultModuleContextualCommand#actionPerformed(java.util.List,
+     *      org.modelio.api.module.IModule)
+     */
+    @Override
+    public void actionPerformed(List<MObject> selectedElements, IModule module) {
+       boolean elementFound = false;
+       ILogService logService = module.getModuleContext().getLogService();
+       logService.info("FindElementInDiagramCommand - actionPerformed(...)");
+
+       IModelingSession session = module.getModuleContext().getModelingSession();
+       List<MObject> root = session.getModel().getModelRoots();
+       IModuleUserConfiguration configuration = module.getModuleContext().getConfiguration();
+
+       ModelElement modelelt = (ModelElement)selectedElements.get(0);               
+        
+       //Pede o input ao user
+       String inputName = JOptionPane.showInputDialog(null, "Insert the name of the element you are searching for:");       
+       
+       //Dá-me os diagramas UML existentes neste projecto
+       Collection<AbstractDiagram> elements = session.findByClass(AbstractDiagram.class);
+       
+       //Iterar todos os diagramas existentes (posso ter dentro deste projecto um class diagram, um state machine, etc...)
+       for(AbstractDiagram d : elements) {
+    	   //Vai buscar os elementos representados neste diagrama
+    	   EList<Element> diagramElements = d.getRepresented();
+    	   //Iterar esses elementos deste diagrama e encontra aquele com o nome igual ao que passámos como input
+    	   for(Element e : diagramElements) {
+    		   
+    		   if(e.getName().equalsIgnoreCase(inputName)) {
+    			   elementFound = true;
+    			   
+    			   if(e.getCompositionOwner() != null) {
+    				   MessageDialog.openInformation(null, "Info", "Owner: " + e.getCompositionOwner());
+    			   }
+    			   
+    			   //Iterar os child objects deste nó (cuidado que pode causar ciclos)
+    			   //CompositionGetter.getAllChildren((Collection<? extends MObject>) e); -> pode evitar ciclos
+    			   List<? extends MObject> compChldrn = e.getCompositionChildren();
+    			   for(MObject c : compChldrn) {
+            	       MessageDialog.openInformation(null, "Info", c.getName() + " " + c.getMClass()); 
+            	       //TODO: Ver se consigo retornar info extra relativamente a este element
+            	       //Provavelmente a solução será fazer uma especie de "parser" onde vou ter de ver se MObject é instanceof e passar os atributos disso
+    			   }
+    			   break;
+    		   }
+    	   }
+
+       }
+       
+       if(!elementFound) {
+		   MessageDialog.openInformation(null, "No Element was found", "Sorry, the element with the name " + inputName + " was not found.");
+	   }     
+       
+       
+       //HashSet rawResults = new HashSet();
+       //rawResults.addAll(session.findByClass(AbstractDiagram.class));
+       
+	   //module.getModuleContext().getModelioServices().getDiagramService().getDiagramHandle(d).getDiagramGraphics(element)
+
+    }
+
+
+
+}
