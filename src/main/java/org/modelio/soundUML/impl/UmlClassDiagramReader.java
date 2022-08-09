e54a714357c083ab9d0e665b84b85accfe2ba115
Guerreiro <n.guerreiro@campus.fct.unl.pt>
2022-08-09 15:44:22 +0100

    Started user dialog
    Maybe Jbutton with addActionListener would be better for this?

--- src/main/java/org/modelio/soundUML/impl/UmlClassDiagramReader.java
+++ src/main/java/org/modelio/soundUML/impl/UmlClassDiagramReader.java
@@ -4,24 +4,30 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.modelio.metamodel.uml.statik.Attribute;
 import org.modelio.vcore.smkernel.mapi.MObject;
 
+
 public class UmlClassDiagramReader {
-	
+
 	private MObject mObject;
 
 	public UmlClassDiagramReader(MObject mObject) {
 		this.mObject = mObject;
 	}
-	
+
 	public void readObject(MObject mObj) {
-		
-		if(mObj instanceof Attribute) {
-			
+
+		if (mObj instanceof Attribute) {
 			String test = ((Attribute) mObj).getType().toString();
 			TextToSpeech.speak(test);
 			MessageDialog.openInformation(null, "Info", test);
+
+			//TODO meter este dialog a fazer algo em cada uma das opções
+			MessageDialog dialog = new MessageDialog(null, "My Title", null, "My message", MessageDialog.ERROR,
+					new String[] { "Play Sound", "Read Message", "Continue" }, 0);
+			int result = dialog.open();
+			System.out.println(result);
+
 		}
-		
-		
+
 	}
-	
+
 }
