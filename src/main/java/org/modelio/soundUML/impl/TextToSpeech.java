c6c549117e83477ad8d2ccc4698945241d743a73
Guerreiro <n.guerreiro@campus.fct.unl.pt>
2022-08-09 15:39:54 +0100

    Changed TTS voice and rate

--- src/main/java/org/modelio/soundUML/impl/TextToSpeech.java
+++ src/main/java/org/modelio/soundUML/impl/TextToSpeech.java
@@ -12,12 +12,12 @@ public class TextToSpeech {
 		//Buscar a voice, se nao isto vai dar nullPointer 
 		System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory"); 
 		
-		voice = VoiceManager.getInstance().getVoice("kevin");// Getting voice
+		voice = VoiceManager.getInstance().getVoice("kevin16");// Getting voice
 		if (voice != null) {
 			voice.allocate();// Allocating Voice
 		}
 		try {
-			voice.setRate(190);// Setting the rate of the voice
+			voice.setRate(130);// Setting the rate of the voice
 			voice.setPitch(150);// Setting the Pitch of the voice
 			voice.setVolume(3);// Setting the volume of the voice
 			voice.speak(text);// Calling speak() method
